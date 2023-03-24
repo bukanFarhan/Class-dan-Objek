6a07b063e14436040ed48c8d90fc29bc141cdde9
muhammad farhan <muhammad2842@students.amikom.ac.idgit config --global user.email muhammad2842@students.amikom.ac.id>
2023-03-24 21:10:28 +0700

    Tugas03 Pemrograman

---
+++ Membuat Class dan Objek/Mobil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Membuat_Class_dan_Objek
+{
+    class Mobil
+    {
+        // property
+        public string warna { get; set; }
+        public string merek { get; set; }
+        public string model { get; set; }
+        public int JumlahPintu { get; set; }
+        public int TahunKeluaran { get; set; }
+        public int kecepatan { get; set; }
+        public string suara { get; set; }
+
+        // methode
+        public void Gas ()
+        {
+            Console.WriteLine("Muhammad Farhan Anggara");
+            Console.WriteLine("19.11.2842");
+            Console.WriteLine("");
+            Console.Write("Mobil {0}", model);
+            Console.Write(" Berjalan dengan kecepatan {0}", kecepatan);
+            Console.WriteLine("\n");
+        }
+
+        public void Klakson()
+        {
+            Console.Write("Suara Klakson {0}", suara);
+            Console.WriteLine("\n");
+        }
+
+        public void TampilkanInfo()
+        {
+            Console.Write("Mobil saya berwarna {0}", warna);
+            Console.Write(" merek {0}", merek);
+            Console.Write(" model {0}", model);
+            Console.Write(" Keluaran tahun {0}", TahunKeluaran);
+            Console.Write(" dengan jumlah pintu sebanyak {0}", JumlahPintu);
+            Console.Write("\n");
+
+        }
+
+    }
+}
---
+++ Membuat Class dan Objek/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Membuat_Class_dan_Objek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mobil mobil1 = new Mobil();
+            Mobil mobil2 = new Mobil();
+            Mobil mobil3 = new Mobil();
+
+            mobil1.warna = "Hitam";
+            mobil1.JumlahPintu = 4;
+            mobil1.merek = "Toyota";
+            mobil1.model = "Kijang Innova";
+            mobil1.TahunKeluaran = 2018;
+            mobil1.kecepatan = 2000;
+            mobil1.suara = "Embeeeee...";
+
+            mobil1.Gas();
+            mobil1.Klakson();
+            mobil1.TampilkanInfo();
+        }
+    }
+}
